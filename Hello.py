122067276e454eb76340ae3b2c007f2a564b2b3b
Mortezayazdanich <56220550+Mortezayazdanich@users.noreply.github.com>
2023-03-02 16:55:22 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,11 +9,19 @@ print("Hello,", name)
 print("Hello, " + name)
 
 #4th Code "Named Parameters"
-# Print doc ==> print(*objects, sep=' ', end='\n', file = sys.stdout, flush = False)
+    # Print doc ==> print(*objects, sep=' ', end='\n', file = sys.stdout, flush = False)
 print("Hello,", end= ' ')
 print(name)
-# sep
+    # sep
 print("Hello", name, sep="|||")
 
 #5th Code "Formatted String"
+# name = name.strip()  #==> Removes Whitespace from str
+# name = name.capitalize() #==> Capitalizes The firs word str
+# name = name.title() #==> Capitalizes Every Word in str
+name = name.strip().title() #==> Removes Whitespace from str and Capitalizes Every Word in str
 print(f"Hello, {name}")
+
+#split Users name into first name and last name
+first_name, last_name = name.split(" ")
+print(f"firstname : {first_name}, last name: {last_name}")
